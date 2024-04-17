d6349145654f97b724980a0f9292a17d019680d7
vaultmicroSeongwoo <you3667@vaultmicro.com>
2024-04-17 18:23:36 +0900

    temp: not-working

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,17 @@
 import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
+import { AbstractIntlMessages } from "next-intl";
 
 // Can be imported from a shared config
 const locales = ["en", "ko"];
 
 export default getRequestConfig(async ({ locale }) => {
-  // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
-
-  return {
-    messages: (await import(`../messages/${locale}.json`)).default,
+  const messages = {
+    ...(await import(`../messages/${locale}/about.json`)).default,
+    ...(await import(`../messages/${locale}/home.json`)).default,
+    ...(await import(`../messages/${locale}/room.json`)).default,
   };
+  // Return merged messages
+  return messages as AbstractIntlMessages;
 });
