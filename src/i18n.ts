18feadea74e11e32d8cff6d0aba52a4aabfe1617
IngEoGeoBuk <you3667@naver.com>
2024-06-29 17:55:27 +0900

    feat: success to split json

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,17 +1,17 @@
 import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
-import { AbstractIntlMessages } from "next-intl";
 
 // Can be imported from a shared config
 const locales = ["en", "ko"];
 
 export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale as any)) notFound();
-  const messages = {
-    ...(await import(`../messages/${locale}/about.json`)).default,
-    ...(await import(`../messages/${locale}/home.json`)).default,
-    ...(await import(`../messages/${locale}/room.json`)).default,
+
+  return {
+    messages: {
+      ...(await import(`../messages/${locale}/about.json`)),
+      ...(await import(`../messages/${locale}/home.json`)),
+      ...(await import(`../messages/${locale}/room.json`)),
+    },
   };
-  // Return merged messages
-  return messages as AbstractIntlMessages;
 });
